cb30bf77ccf913036aeea723a48bd374c951eee3
Chau Hoang Tue Man <tueman812017@gmail.com>
2024-09-04 01:18:02 +0700

    Upload Lab03_01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.lab3_01"
-        minSdk = 28
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
